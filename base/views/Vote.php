f24c7938a08375f9e5dedd713235cf14ff5830f0
Outer Haven, LLC <117410279+OuterHavenLLC@users.noreply.github.com>
2023-06-18 15:45:58 -0400

    Established the broad layout for the Vote processor.

--- base/views/Vote.php
+++ base/views/Vote.php
@@ -8,7 +8,7 @@
    $data = $a["Data"] ?? [];
    $id = $data["ID"] ?? "";
    $r = $this->system->Element([
-    "p", "Missing Vote ID.", ["class" => "CenterText"]
+    "p", "Missing Vote ID", ["class" => "CenterText InnerMargin"]
    ]);
    $refresh = $data["Refresh"] ?? 0;
    $type = $data["Type"] ?? 1;
@@ -29,15 +29,16 @@
     $class = ($type == 2) ? "" : $class;
     $class = ($type == 3) ? "Desktop66 " : $class;
     $class = ($type == 4) ? "" : $class;
+    $processor = "v=".base64_encode("Vote:Save")."&Type=$type&Vote=";
     $voteDown = ($_Votes[$you] == "Down") ? "BBB " : "";
     $voteUp = ($_Votes[$you] == "Up") ? "BBB " : "";
     $votes = $_VoteUp - $_VoteDown;
     $r = $this->system->Change([[
      "[Vote.Down]" => $voteDown,
-     "[Vote.Down.Processor]" => "",
+     "[Vote.Down.Processor]" => $processor."Down",
      "[Vote.ID]" => $id,
      "[Vote.Up]" => $voteUp,
-     "[Vote.Up.Processor]" => "",
+     "[Vote.Up.Processor]" => $processor."Up",
      "[Vote.Total]" => $this->system->ShortNumber($votes)
     ], $this->system->Page("39a550decb7f3f764445b33e847a7042")]);
     $r = ($refresh == 0) ? $this->system->Element(["div", $r, [
@@ -50,11 +51,18 @@
    $data = $a["Data"] ?? [];
    $id = $data["ID"] ?? "";
    $r = $this->system->Element([
-    "p", "Missing Vote ID.", ["class" => "CenterText"]
+    "p", "Missing Vote ID", ["class" => "CenterText InnerMargin"]
    ]);
    $type = $data["Type"] ?? "";
-   // RECORD OR REMOVE VOTE
-   // REFRESH INNER CONTAINER
+   $vote = $data["Vote"] ?? "";
+   if(!empty($id)) {
+    // RECORD OR REMOVE VOTE
+    $r = $this->view(base64_encode("Vote:Containers"), ["Data" => [
+     "ID" => $id,
+     "Refresh" => 1,
+     "Type" => $type
+    ]]);
+   }
    return $r;
   }
   function __destruct() {
