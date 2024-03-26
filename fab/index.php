f48aeeedbf7f5285030b3916a2c71ec82887d0ba
Outer Haven, LLC <117410279+OuterHavenLLC@users.noreply.github.com>
2024-03-26 14:19:21 -0400

    The Public Event view is now rendered if there is an active event and the Broadcast view is enabled.

--- fab/index.php
+++ fab/index.php
@@ -1,18 +1,32 @@
 <?php
  require_once("/var/www/html/base/Bootloader.php");
+ $activeEvent = 0;
+ $broadcastViewEnabled = 0;
  $oh = New OH;
- $event = $oh->core->config["App"]["LiveEvent"] ?? 0;
- # LOOP THROUGH EVENTS AND CHECK FOR AN ACTIVE EVENT
- $extension = ($event == 1) ? "1870885288027c3d4bc0a29bdf5f7579" : "c0f79632dc2313352f92b41819fe4739";
+ $description = $oh->core->config["App"]["Description"] ?? "";
+ $events = $oh->core->config["App"]["PublicEvents"] ?? [];
+ $selectedEvent = [];
+ $title = "Free America Broadcasting";
+ foreach($events as $event => $info) {
+  if($info["Active"] == 1) {
+   $activeEvent = 1;
+   $broadcastViewEnabled = $info["EnablePublicBroadcast"]
+   $selectedEvent = $info;
+  }
+ } if($broadcastViewEnabled == 1) {
+  $description = $selectedEvent["Description"] ?? $description;
+  $title = $selectedEvent["Title"] ?? $title;
+ }
+ $extension = ($activeEvent == 1) ? "1870885288027c3d4bc0a29bdf5f7579" : "c0f79632dc2313352f92b41819fe4739";
  echo $oh->core->Change([[
   "[App.Content]" => $oh->core->Change([[
    "[FAB.Chat]" => base64_encode("v=".base64_encode("Chat:Home")."&Card=1&Group=1&ID=7216072bbd437563e692cc7ff69cdb69"),
    "[FAB.Listen]" => base64_encode("v=".base64_encode("Subscription:FABPlayer"))
   ], $oh->core->Extension($extension)]),
-  "[App.Description]" => $oh->core->config["App"]["Description"],
+  "[App.Description]" => $description,
   "[App.Keywords]" => $oh->core->config["App"]["Keywords"],
   "[App.Owner]" => $oh->core->ShopID,
-  "[App.Title]" => "Free America Broadcasting"
+  "[App.Title]" => $title
  ], $oh->core->PlainText([
   "BBCodes" => 1,
   "Data" => file_get_contents("./index.txt"),
