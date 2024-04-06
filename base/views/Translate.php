179e71a46ccb4779c395e2730d4c7e72437c0438
Outer Haven, LLC <117410279+OuterHavenLLC@users.noreply.github.com>
2024-04-06 13:19:09 -0400

    Created the first draft for the revised Translate inputs clone.

--- base/views/Translate.php
+++ base/views/Translate.php
@@ -27,83 +27,16 @@
      "p", "Translate is disabled for this experience."
     ]);
    } elseif(!empty($id)) {
-    $_Regions = $this->core->Extension("5f6ea04c169f32041a39e16d20f95a05");
-    $_Translations = $this->core->Extension("63dde5af1a1ec0968ccb006248b55f48");
     $accessCode = "Accepted";
     $id = base64_decode($id);
-    $locals = "";
-    $lt = $this->core->Data("Get", ["translate", $id]) ?? [];
-    $regions = "";
-    if(empty($lt)) {
-     $k = md5($you."_Local_".$this->core->timestamp);
-     $code = "&#91;Languages:$id-$k&#93;";
-     foreach($this->core->Languages() as $re => $la) {
-      $t = $lt[$k][$re] ?? "";
-      $t = (!empty($t)) ? $this->core->PlainText([
-       "Data" => $t,
-       "Decode" => 1,
-       "HTMLDecode" => 1
-      ]) : $t;
-      $regions .= $this->core->Change([[
-       "[Region.Language]" => $la,
-       "[Region.LocalID]" => $k,
-       "[Region.Code]" => $re,
-       "[Region.Text]" => $t
-      ], $_Regions]);
-     }
-     $translations .= $this->core->Change([[
-      "[Local.Code]" => $code,
-      "[Local.ID]" => $k,
-      "[Local.Regions]" => $regions
-     ], $_Translations]);
-    } else {
-     foreach($lt as $k => $v) {
-      $code = "&#91;Languages:$id-$k&#93;";
-      foreach($this->core->Languages() as $re => $la) {
-       $t = $v[$re] ?? "";
-       $t = (!empty($t)) ? $this->core->PlainText([
-        "Data" => $t,
-        "Decode" => 1,
-        "HTMLDecode" => 1
-       ]) : $t;
-       $regions .= $this->core->Change([[
-        "[Region.Language]" => $la,
-        "[Region.LocalID]" => $k,
-        "[Region.Code]" => $re,
-        "[Region.Text]" => $t
-       ], $_Regions]);
-      }
-      $translations .= $this->core->Change([[
-       "[Local.Code]" => $code,
-       "[Local.ID]" => $k,
-       "[Local.Regions]" => $regions
-      ], $_Translations]);
-     }
-    } foreach($this->core->Languages() as $re => $la) {
-     $regions .= $this->core->Change([[
-      "[Region.Language]" => $la,
-      "[Region.LocalID]" => "LocalID",
-      "[Region.Code]" => $re,
-      "[Region.Text]" => ""
-     ], $_Regions]);
-    }
-    $_Trsnalations = $this->core->PlainText([
-     "Data" => $this->core->Change([[
-      "[Local.Code]" => "LocalCode",
-      "[Local.ID]" => "LocalID",
-      "[Local.Regions]" => $regions
-     ], $_Translations]),
-     "HTMLEncode" => 1
-    ]);
+    $translations = $this->core->Data("Get", ["translate", $id]) ?? [];
+    /*--foreach($translations as $textID => $translation) {
+    }--*/
     $r = $this->core->Change([[
-     "[Languages.CloneVariables]" => base64_encode(json_encode([
-      "LocalCode" => htmlentities("[Languages:$id-LocalID]"),
-      "LocalID" => "GenerateUniqueID"
-     ])),
-     "[Languages.CloneTPL]" => $_Translations,
-     "[Languages.ID]" => $id,
-     "[Languages.Locals]" => $translations,
-     "[Languages.Processor]" => base64_encode("v=".base64_encode("Translate:Save")),
+     "[Trsnalate.Clone]" => base64_encode($this->RenderClone()),
+     "[Trsnalate.ID]" => $id,
+     "[Trsnalate.Translations]" => json_encode($translations, true),
+     "[Trsnalate.Save]" => base64_encode("v=".base64_encode("Translate:Save")),
     ], $this->core->Extension("d4ccf0731cd5ee5c10c04a9193bd61d5")]);
    }
    return $this->core->JSONResponse([
@@ -115,11 +48,17 @@
     "ResponseType" => "View"
    ]);
   }
+  function RenderClone() {
+   $_Regions = $this->core->Extension("5f6ea04c169f32041a39e16d20f95a05");
+   $_Translations = $this->core->Extension("63dde5af1a1ec0968ccb006248b55f48");
+   $clone = "{TRANSLATIONS_CLONE}";
+   return $clone;
+  }
   function Save(array $a) {
    $accessCode = "Denied";
    $data = $a["Data"] ?? [];
    $data = $this->core->DecodeBridgeData($data);
-   $data = $this->core->FixMissing($data, ["ID"]);
+   $id = $data["ID"] ?? "";
    $r = [
     "Body" => "The Translation Package Identirifer is missing."
    ];
@@ -130,9 +69,9 @@
      "Body" => "You must be signed in to continue.",
      "Header" => "Forbidden"
     ];
-   } elseif(!empty($data["ID"])) {
+   } elseif(!empty($id)) {
     $accessCode = "Accepted";
-    $lt = $this->core->Data("Get", ["translate", $data["ID"]]);
+    $lt = $this->core->Data("Get", ["translate", $id]) ?? [];
     $lt = $lt ?? [];
     foreach($d as $k => $v) {
      if(strpos($k, "Locals_") !== false) {
@@ -147,11 +86,11 @@
       }
      }
     }
+    #$this->core->Data("Save", ["translate", $data["ID"], $lt]);
     $r = [
      "Body" => "The Localization was saved.",
      "Header" => "Done"
     ];
-    #$this->core->Data("Save", ["translate", $data["ID"], $lt]);
    }
    return $this->core->JSONResponse([
     "AccessCode" => $accessCode,
