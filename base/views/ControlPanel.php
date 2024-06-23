bf5ae26f941fb4de7824893681737a144c31f762
Outer Haven, LLC <117410279+OuterHavenLLC@users.noreply.github.com>
2024-06-22 20:34:43 -0400

    The Control Panel is now protected by PIN.

--- base/views/ControlPanel.php
+++ base/views/ControlPanel.php
@@ -23,121 +23,147 @@
     $_Search = base64_encode("Search:Containers");
     $accessCode = "Accepted";
     $config = $this->core->config ?? [];
-    $events = "";
-    $eventsList = $config["PublicEvents"] ?? [];
-    $media = "";
-    $mediaList = $config["Media"] ?? [];
-    $previewQuantity = base64_encode("Single");
-    $responseType = "View";
-    $saveFirst = base64_encode("v=".base64_encode("ControlPanel:SaveFirst"));
-    $search = "";
-    $searchLists = $config["App"]["Search"] ?? [];
-    $shopID = $config["App"]["ShopID"] ?? "Mike";
-    $statistics = "";
-    $statisticsList = $config["Statistics"] ?? [];
-    foreach($mediaList as $key => $info) {
-     $addTo = base64_encode("Link to ".$info["Name"].":.AddTo$key");
-     $added = base64_encode("Added! Feel free to close this card.");
-     $file = (!empty($info["File"])) ? base64_encode($info["File"]) : "";
-     $media .= $this->core->Change([[
-      "[Clone.ID]" => $key,
-      "[Media.Add]" => base64_encode("v=".base64_encode("Search:Containers")."&CARD=1&st=XFS&AddTo=$addTo&Added=$added&UN=".base64_encode($this->core->ID)),
-      "[Media.File]" => $file,
-      "[Media.File.Quantity]" => $previewQuantity,
-      "[Media.ID]" => $key,
-      "[Media.Input]" => "MediaFile[]",
-      "[Media.Input.LiveView]" => $_LiveView,
-      "[Media.Name]" => $info["Name"]
-     ], $this->core->Extension("f1a8c31050b241ebcea22f33cf6171f4")]);
-    } foreach($eventsList as $event => $info) {
-     $addTo = base64_encode("Set as ".$info["Title"]."'s Cover Photo:.AddTo$event");
-     $added = base64_encode("Added! Feel free to close this card.");
-     $coverPhoto = (!empty($info["CoverPhoto"])) ? base64_encode($info["CoverPhoto"]) : "";
-     $domains_base = $config["App"]["Domains_Base"] ?? "outerhaven.nyc";
-     $domains_fileSystem = $config["App"]["Domains_FileSystem"] ?? "efs.outerhaven.nyc";
-     $events .= $this->core->Change([[
-      "[Clone.ID]" => $event,
-      "[Event.BannerText]" => $info["BannerText"],
-      "[Event.Description]" => $info["Description"],
-      "[Event.ID]" => $event,
-      "[Event.Link]" => $info["Link"],
-      "[Event.Title]" => $info["Title"],
-      "[Media.Add]" => base64_encode("v=".base64_encode("Search:Containers")."&CARD=1&st=XFS&AddTo=$addTo&Added=$added&ftype=".base64_encode(json_encode(["Photo"]))."&UN=".base64_encode($this->core->ID)),
-      "[Media.File]" => $coverPhoto,
-      "[Media.File.Quantity]" => $previewQuantity,
-      "[Media.Input]" => "EventCoverPhoto[]",
-      "[Media.Input.LiveView]" => $_LiveView
-     ], $this->core->Extension("889a3f39fa958bcc2a57b2f1882198ff")]);
-    } foreach($searchLists as $list => $info) {
-     $search .= $this->core->Change([[
-      "[Clone.ID]" => $list,
-      "[List.Description]" => $info["Description"],
-      "[List.ID]" => $list,
-      "[List.Title]" => $info["Title"]
-     ], $this->core->Extension("3777f71aa914041840ead48e3a259866")]);
-    } foreach($statisticsList as $stat => $name) {
-     $statistics .= $this->core->Change([[
-      "[Clone.ID]" => $stat,
-      "[Statistic.ID]" => $stat,
-      "[Statistic.Name]" => $name
-     ], $this->core->Extension("21af4585b38e4b15a37fce7dfbb95161")]);
+    $verifyPassPhrase = $data["VerifyPassPhrase"] ?? 0;
+    $r = $this->view(base64_encode("Authentication:ProtectedContent"), ["Data" => [
+     "Header" => base64_encode($this->core->Element([
+      "h1", "Control Panel", ["class" => "CenterText"]
+     ])),
+     "Text" => base64_encode("Please enter your PIN to access the <em>".$config["App"]["Name"]."</em> configuration."),
+     "ViewData" => base64_encode(json_encode([
+      "SecureKey" => base64_encode($y["Login"]["PIN"]),
+      "VerifyPassPhrase" => 1,
+      "v" => base64_encode("ControlPanel:Home")
+     ], true))
+    ]]);
+    $r = $this->core->RenderView($r);
+    if($verifyPassPhrase == 1) {
+     $accessCode = "Denied";
+     $key = $data["Key"] ?? base64_encode("");
+     $key = base64_decode($key);
+     $r = $this->core->Element(["p", "The Key is missing."]);
+     $secureKey = $data["SecureKey"] ?? base64_encode("");
+     $secureKey = base64_decode($secureKey);
+     if(md5($key) != $secureKey) {
+      $r = $this->core->Element(["p", "The Keys do not match."]);
+     } else {
+      $accessCode = "Accepted";
+      $events = "";
+      $eventsList = $config["PublicEvents"] ?? [];
+      $media = "";
+      $mediaList = $config["Media"] ?? [];
+      $previewQuantity = base64_encode("Single");
+      $responseType = "View";
+      $saveFirst = base64_encode("v=".base64_encode("ControlPanel:SaveFirst"));
+      $search = "";
+      $searchLists = $config["App"]["Search"] ?? [];
+      $shopID = $config["App"]["ShopID"] ?? "Mike";
+      $statistics = "";
+      $statisticsList = $config["Statistics"] ?? [];
+      foreach($mediaList as $key => $info) {
+       $addTo = base64_encode("Link to ".$info["Name"].":.AddTo$key");
+       $added = base64_encode("Added! Feel free to close this card.");
+       $file = (!empty($info["File"])) ? base64_encode($info["File"]) : "";
+       $media .= $this->core->Change([[
+        "[Clone.ID]" => $key,
+        "[Media.Add]" => base64_encode("v=".base64_encode("Search:Containers")."&CARD=1&st=XFS&AddTo=$addTo&Added=$added&UN=".base64_encode($this->core->ID)),
+        "[Media.File]" => $file,
+        "[Media.File.Quantity]" => $previewQuantity,
+        "[Media.ID]" => $key,
+        "[Media.Input]" => "MediaFile[]",
+        "[Media.Input.LiveView]" => $_LiveView,
+        "[Media.Name]" => $info["Name"]
+       ], $this->core->Extension("f1a8c31050b241ebcea22f33cf6171f4")]);
+      } foreach($eventsList as $event => $info) {
+       $addTo = base64_encode("Set as ".$info["Title"]."'s Cover Photo:.AddTo$event");
+       $added = base64_encode("Added! Feel free to close this card.");
+       $coverPhoto = (!empty($info["CoverPhoto"])) ? base64_encode($info["CoverPhoto"]) : "";
+       $domains_base = $config["App"]["Domains_Base"] ?? "outerhaven.nyc";
+       $domains_fileSystem = $config["App"]["Domains_FileSystem"] ?? "efs.outerhaven.nyc";
+       $events .= $this->core->Change([[
+        "[Clone.ID]" => $event,
+        "[Event.BannerText]" => $info["BannerText"],
+        "[Event.Description]" => $info["Description"],
+        "[Event.ID]" => $event,
+        "[Event.Link]" => $info["Link"],
+        "[Event.Title]" => $info["Title"],
+        "[Media.Add]" => base64_encode("v=".base64_encode("Search:Containers")."&CARD=1&st=XFS&AddTo=$addTo&Added=$added&ftype=".base64_encode(json_encode(["Photo"]))."&UN=".base64_encode($this->core->ID)),
+        "[Media.File]" => $coverPhoto,
+        "[Media.File.Quantity]" => $previewQuantity,
+        "[Media.Input]" => "EventCoverPhoto[]",
+        "[Media.Input.LiveView]" => $_LiveView
+       ], $this->core->Extension("889a3f39fa958bcc2a57b2f1882198ff")]);
+      } foreach($searchLists as $list => $info) {
+       $search .= $this->core->Change([[
+        "[Clone.ID]" => $list,
+        "[List.Description]" => $info["Description"],
+        "[List.ID]" => $list,
+        "[List.Title]" => $info["Title"]
+       ], $this->core->Extension("3777f71aa914041840ead48e3a259866")]);
+      } foreach($statisticsList as $stat => $name) {
+       $statistics .= $this->core->Change([[
+        "[Clone.ID]" => $stat,
+        "[Statistic.ID]" => $stat,
+        "[Statistic.Name]" => $name
+       ], $this->core->Extension("21af4585b38e4b15a37fce7dfbb95161")]);
+      }
+      $r = $this->core->Change([[
+       "[Admin.Domain]" => "W('https://www.godaddy.com/', '_blank');",
+       "[Admin.Feedback]" => base64_encode("v=$_Search&st=Feedback"),
+       "[Admin.Files]" => base64_encode("v=".base64_encode("Album:List")."&AID=".md5("unsorted")."&UN=".base64_encode($this->core->ID)),
+       "[Admin.MassMail]" => base64_encode("v=$_Search&st=ADM-MassMail"),
+       "[Admin.Mail]" => "https://mail.outerhaven.nyc/iredadmin/",
+       "[Admin.Pages]" => base64_encode("v=$_Search&CARD=1&st=ADM-LLP"),
+       "[Admin.RenewSubscriptions]" => base64_encode("v=".base64_encode("Subscription:RenewAll")),
+       "[Admin.Server]" => "https://www.digitalocean.com/",
+       "[Configuration.App.Description]" => base64_encode($config["App"]["Description"]),
+       "[Configuration.App.Domains.Base]" => base64_encode($domains_base),
+       "[Configuration.App.Domains.FileSystem]" => base64_encode($domains_fileSystem),
+       "[Configuration.App.Keywords]" => base64_encode($config["App"]["Keywords"]),
+       "[Configuration.App.Maintenance]" => $config["Maintenance"],
+       "[Configuration.App.Name]" => base64_encode($config["App"]["Name"]),
+       "[Configuration.App.ShopID]" => base64_encode($shopID),
+       "[Configuration.Events]" => $events,
+       "[Configuration.Events.Clone]" => base64_encode($this->core->Change([[
+        "[Event.BannerText]" => "",
+        "[Event.Description]" => "",
+        "[Event.ID]" => "",
+        "[Event.Link]" => "",
+        "[Event.Title]" => "",
+        "[Media.Add]" => $saveFirst,
+        "[Media.File]" => "",
+        "[Media.File.Quantity]" => $previewQuantity,
+        "[Media.Input]" => "EventCoverPhoto[]",
+        "[Media.Input.LiveView]" => $_LiveView
+       ], $this->core->Extension("889a3f39fa958bcc2a57b2f1882198ff")])),
+       "[Configuration.Media]" => $media,
+       "[Configuration.Media.Clone]" => base64_encode($this->core->Change([[
+        "[Media.Add]" => $saveFirst,
+        "[Media.File]" => "",
+        "[Media.File.Quantity]" => $previewQuantity,
+        "[Media.Input]" => "MediaFile[]",
+        "[Media.Input.LiveView]" => $_LiveView,
+        "[Media.ID]" => "",
+        "[Media.Name]" => ""
+       ], $this->core->Extension("f1a8c31050b241ebcea22f33cf6171f4")])),
+       "[Configuration.Save.App]" => base64_encode("v=".base64_encode("ControlPanel:SaveApp")),
+       "[Configuration.Save.Events]" => base64_encode("v=".base64_encode("ControlPanel:SaveEvents")),
+       "[Configuration.Save.Media]" => base64_encode("v=".base64_encode("ControlPanel:SaveMedia")),
+       "[Configuration.Save.Search]" => base64_encode("v=".base64_encode("ControlPanel:SaveSearch")),
+       "[Configuration.Save.Statistics]" => base64_encode("v=".base64_encode("ControlPanel:SaveStatistics")),
+       "[Configuration.Search]" => $search,
+       "[Configuration.Search.Clone]" => base64_encode($this->core->Change([[
+        "[List.Description]" => "",
+        "[List.ID]" => "",
+        "[List.Title]" => ""
+       ], $this->core->Extension("3777f71aa914041840ead48e3a259866")])),
+       "[Configuration.Statistics]" => $statistics,
+       "[Configuration.Statistics.Clone]" => base64_encode($this->core->Change([[
+        "[Statistic.ID]" => "",
+        "[Statistic.Name]" => ""
+       ], $this->core->Extension("21af4585b38e4b15a37fce7dfbb95161")]))
+      ], $this->core->Extension("5c1ce5c08e2add4d1487bcd2193315a7")]);
+     }
     }
-    $r = $this->core->Change([[
-     "[Admin.Domain]" => "W('https://www.godaddy.com/', '_blank');",
-     "[Admin.Feedback]" => base64_encode("v=$_Search&st=Feedback"),
-     "[Admin.Files]" => base64_encode("v=".base64_encode("Album:List")."&AID=".md5("unsorted")."&UN=".base64_encode($this->core->ID)),
-     "[Admin.MassMail]" => base64_encode("v=$_Search&st=ADM-MassMail"),
-     "[Admin.Mail]" => "https://mail.outerhaven.nyc/iredadmin/",
-     "[Admin.Pages]" => base64_encode("v=$_Search&CARD=1&st=ADM-LLP"),
-     "[Admin.RenewSubscriptions]" => base64_encode("v=".base64_encode("Subscription:RenewAll")),
-     "[Admin.Server]" => "https://www.digitalocean.com/",
-     "[Configuration.App.Description]" => base64_encode($config["App"]["Description"]),
-     "[Configuration.App.Domains.Base]" => base64_encode($domains_base),
-     "[Configuration.App.Domains.FileSystem]" => base64_encode($domains_fileSystem),
-     "[Configuration.App.Keywords]" => base64_encode($config["App"]["Keywords"]),
-     "[Configuration.App.Maintenance]" => $config["Maintenance"],
-     "[Configuration.App.Name]" => base64_encode($config["App"]["Name"]),
-     "[Configuration.App.ShopID]" => base64_encode($shopID),
-     "[Configuration.Events]" => $events,
-     "[Configuration.Events.Clone]" => base64_encode($this->core->Change([[
-      "[Event.BannerText]" => "",
-      "[Event.Description]" => "",
-      "[Event.ID]" => "",
-      "[Event.Link]" => "",
-      "[Event.Title]" => "",
-      "[Media.Add]" => $saveFirst,
-      "[Media.File]" => "",
-      "[Media.File.Quantity]" => $previewQuantity,
-      "[Media.Input]" => "EventCoverPhoto[]",
-      "[Media.Input.LiveView]" => $_LiveView
-     ], $this->core->Extension("889a3f39fa958bcc2a57b2f1882198ff")])),
-     "[Configuration.Media]" => $media,
-     "[Configuration.Media.Clone]" => base64_encode($this->core->Change([[
-      "[Media.Add]" => $saveFirst,
-      "[Media.File]" => "",
-      "[Media.File.Quantity]" => $previewQuantity,
-      "[Media.Input]" => "MediaFile[]",
-      "[Media.Input.LiveView]" => $_LiveView,
-      "[Media.ID]" => "",
-      "[Media.Name]" => ""
-     ], $this->core->Extension("f1a8c31050b241ebcea22f33cf6171f4")])),
-     "[Configuration.Save.App]" => base64_encode("v=".base64_encode("ControlPanel:SaveApp")),
-     "[Configuration.Save.Events]" => base64_encode("v=".base64_encode("ControlPanel:SaveEvents")),
-     "[Configuration.Save.Media]" => base64_encode("v=".base64_encode("ControlPanel:SaveMedia")),
-     "[Configuration.Save.Search]" => base64_encode("v=".base64_encode("ControlPanel:SaveSearch")),
-     "[Configuration.Save.Statistics]" => base64_encode("v=".base64_encode("ControlPanel:SaveStatistics")),
-     "[Configuration.Search]" => $search,
-     "[Configuration.Search.Clone]" => base64_encode($this->core->Change([[
-      "[List.Description]" => "",
-      "[List.ID]" => "",
-      "[List.Title]" => ""
-     ], $this->core->Extension("3777f71aa914041840ead48e3a259866")])),
-     "[Configuration.Statistics]" => $statistics,
-     "[Configuration.Statistics.Clone]" => base64_encode($this->core->Change([[
-      "[Statistic.ID]" => "",
-      "[Statistic.Name]" => ""
-     ], $this->core->Extension("21af4585b38e4b15a37fce7dfbb95161")]))
-    ], $this->core->Extension("5c1ce5c08e2add4d1487bcd2193315a7")]);
    }
    return $this->core->JSONResponse([
     "AccessCode" => $accessCode,
