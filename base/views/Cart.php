82f7cfa9ef4ef0cb33875b6020f5e9f0d1038c2c
Outer Haven, LLC <117410279+OuterHavenLLC@users.noreply.github.com>
2025-04-17 15:16:27 -0400

    The Cart view group was updated to current standards.

--- base/views/Cart.php
+++ base/views/Cart.php
@@ -4,28 +4,27 @@
    parent::__construct();
    $this->you = $this->core->Member($this->core->Authenticate("Get"));
   }
-  function Add(array $a) {
-   $accessCode = "Accepted";
-   $data = $a["Data"] ?? [];
-   $data = $this->core->FixMissing($data, ["ID", "T"]);
-   $id = $data["ID"];
-   $r = $this->core->Element([
+  function Add(array $data) {
+   $_Dialog = $this->core->Element([
     "p", "You must be signed in to make purchases.", ["class" => "CenterText"]
    ]);
+   $_View = "";
+   $data = $data["Data"] ?? [];
+   $id = $data["ID"] ?? "";
    $y = $this->you;
    $you = $y["Login"]["Username"];
    if(!empty($data["T"]) && $this->core->ID != $you) {
     $sub = $y["Subscriptions"]["Artist"]["A"] ?? 0;
     $t = ($data["T"] == $you) ? $y : $this->core->Member($data["T"]);
-    $shop = $this->core->Data("Get", ["shop", md5($t["Login"]["Username"])]) ?? [];
+    $shop = $this->core->Data("Get", ["shop", md5($t["Login"]["Username"])]);
     if($sub == 0 && $id == "c7054e9c7955203b721d142dedc9e540") {
-     $accessCode = "Accepted";
-     $r = [
+     $_Dialog = [
       "Body" => "Pay your commisiion via the Subscriptions page, and you will automatically be subscribed.",
       "Header" => "Commission Due"
      ];
     } else {
-     $product = $this->core->Data("Get", ["product", $id]) ?? [];
+     $_Dialog = "";
+     $product = $this->core->Data("Get", ["product", $id]);
      $category = $product["Category"] ?? "";
      $cost = $product["Cost"] ?? 0;
      $hasInstructions = $product["Instructions"] ?? "";
@@ -75,69 +74,81 @@
        "Type" => "Text",
        "Value" => 1
       ];
-      $r = $this->core->Change([[
-       "[AddToCart.Data]" => base64_encode("v=".base64_encode("Cart:SaveAdd")),
-       "[AddToCart.Product.ID]" => $id,
-       "[AddToCart.Product.Instructions]" => $instructions,
-       "[AddToCart.Product.LowStock]" => $lowStock,
-       "[AddToCart.Product.Price]" => number_format($price, 2),
-       "[AddToCart.Product.Quantity]" => json_encode([
-        $quantity
-       ], true),
-       "[AddToCart.Shop.ID]" => md5($t["Login"]["Username"]),
-       "[AddToCart.Shop.Owner]" => $t["Login"]["Username"]
-      ], $this->core->Extension("624bcc664e9bff0002e01583e7706d83")]);
+      $_View = [
+       "ChangeData" => [
+        "[AddToCart.Data]" => base64_encode("v=".base64_encode("Cart:SaveAdd")),
+        "[AddToCart.Product.ID]" => $id,
+        "[AddToCart.Product.Instructions]" => $instructions,
+        "[AddToCart.Product.LowStock]" => $lowStock,
+        "[AddToCart.Product.Price]" => number_format($price, 2),
+        "[AddToCart.Product.Quantity]" => json_encode([
+         $quantity
+        ], true),
+        "[AddToCart.Shop.ID]" => md5($t["Login"]["Username"]),
+        "[AddToCart.Shop.Owner]" => $t["Login"]["Username"]
+       ],
+       "ExtensionID" => "624bcc664e9bff0002e01583e7706d83"
+      ];
       if(($category == "Product") && $t["Login"]["Username"] == $you) {
-       $r = $this->core->Element([
-        "p", "Deliverable Products are disabled as you own this shop.$r",
-        ["class" => "CenterText"]
-       ]);
+       $_View = [
+        "ChangeData" => [],
+        "Extension" => $this->core->AESencrypt($this->core->Element([
+         "p", "Deliverable Products are disabled as you own this shop.",
+         ["class" => "CenterText"]
+        ]))
+       ];
       } elseif($category == "Subscription") {
-       $sub = $this->core->Element([
-        "h4", "Already Subscribed",
-        ["class" => "UpperCase CenterText"]
-       ]);
+       $_Subscribed = [
+        "ChangeData" => [],
+        "Extension" => $this->core->AESencrypt($this->core->Element([
+         "h4", "Already Subscribed",
+         ["class" => "UpperCase CenterText"]
+        ]))
+       ];
        if($id == "e4302295d2812e4f374ef1035891c4d1") {
-        $r = ($y["Subscriptions"]["Developer"]["A"] == 1) ? $sub : $r;
+        $_View = ($y["Subscriptions"]["Developer"]["A"] == 1) ? $_Subscribed : $_View;
        } elseif($id == "c7054e9c7955203b721d142dedc9e540") {
-        $r = ($y["Subscriptions"]["Artist"]["A"] == 1) ? $sub : $r;
+        $_View = ($y["Subscriptions"]["Artist"]["A"] == 1) ? $_Subscribed : $_View;
        } elseif($id == "cc84143175d6ae2051058ee0079bd6b8") {
-        $r = ($y["Subscriptions"]["Blogger"]["A"] == 1) ? $sub : $r;
+        $_View = ($y["Subscriptions"]["Blogger"]["A"] == 1) ? $_Subscribed : $_View;
        } elseif($id == "355fd2f096bdb49883590b8eeef72b9c") {
-        $r = ($y["Subscriptions"]["VIP"]["A"] == 1) ? $sub : $r;
+        $_View = ($y["Subscriptions"]["VIP"]["A"] == 1) ? $_Subscribed : $_View;
        } elseif($id == "5bfb3f44cdb9d3f2cd969a23f0e37093") {
-        $r = ($y["Subscriptions"]["XFS"]["A"] == 1) ? $sub : $r;
+        $_View = ($y["Subscriptions"]["XFS"]["A"] == 1) ? $_Subscribed : $_View;
        }
       }
      } else {
-      $r = $this->core->Element([
-       "p", "Out of Stock", ["class" => "CenterText"]
-      ]);
+      $_View = [
+       "ChangeData" => [],
+       "Extension" => $this->core->AESencrypt($this->core->Element([
+        "p", "Out of Stock", ["class" => "CenterText"]
+       ]))
+      ];
      }
     }
    }
    return $this->core->JSONResponse([
-    "AccessCode" => $accessCode,
     "AddTopMargin" => "0",
-    "Response" => [
-     "JSON" => "",
-     "Web" => $r
-    ],
-    "ResponseType" => "View"
+    "Dialog" => $_Dialog,
+    "View" => $_View
    ]);
   }
-  function Home(array $a) {
-   $accessCode = "Denied";
-   $data = $a["Data"] ?? [];
-   $r = [
-    "Scrollable" => $this->core->Extension("8b3e21c565a8220fb6eb0a4433fe0739")
+  function Home(array $data) {
+   $_Dialog = [
+    "Body" => "The Shop Identifier is missing."
    ];
+   $data = $data["Data"] ?? [];
    $username = base64_decode($data["UN"]);
    $y = $this->you;
    $you = $y["Login"]["Username"];
    $username = (!empty($username)) ? $username : $you;
-   if($this->core->ID != $username) {
-    $accessCode = "Accepted";
+   if($this->core->ID == $you) {
+    $_Dialog = [
+     "Body" => "You must sign in to continue.",
+     "Header" => "Forbidden"
+    ];
+   } else {
+    $_Dialog = "";
     $t = ($username == $you) ? $y : $this->core->Member($username);
     $id = md5($t["Login"]["Username"]);
     $points = 100000;
@@ -167,39 +178,43 @@
      "p", "<em>".base64_decode($discountCode)."</em> was applied to your order!",
      ["class" => "CenterText"]
     ]);
-    $r = $this->core->Change([[
-     "[Cart.CreditExchange]" => $creditExchange,
-     "[Cart.DiscountCodes]" => $discountCode,
-     "[Cart.List]" => base64_encode("v=".base64_encode("Search:Containers")."&Username=".$t["Login"]["Username"]."&st=CART"),
-     "[Cart.Shop.ID]" => $id,
-     "[Cart.Shop.Title]" => $shop["Title"],
-     "[Cart.Summary]" => base64_encode("v=".base64_encode("Cart:Summary")."&UN=".$data["UN"])
-    ], $this->core->Extension("ac678179fb0fb0c66cd45d738991abb9")]);
+    $_View = [
+     "ChangeData" => [
+      "[Cart.CreditExchange]" => $creditExchange,
+      "[Cart.DiscountCodes]" => $discountCode,
+      "[Cart.List]" => base64_encode("v=".base64_encode("Search:Containers")."&Username=".$t["Login"]["Username"]."&st=CART"),
+      "[Cart.Shop.ID]" => $id,
+      "[Cart.Shop.Title]" => $shop["Title"],
+      "[Cart.Summary]" => base64_encode("v=".base64_encode("Cart:Summary")."&UN=".$data["UN"])
+     ],
+     "ExtensionID" => "ac678179fb0fb0c66cd45d738991abb9"
+    ];
    }
    return $this->core->JSONResponse([
-    "AccessCode" => $accessCode,
     "AddTopMargin" => "0",
-    "Response" => [
-     "JSON" => "",
-     "Web" => $r
-    ],
-    "ResponseType" => "View"
+    "Dialog" => $_Dialog,
+    "View" => $_View
    ]);
   }
-  function SaveAdd(array $a) {
-   $accessCode = "Denied";
+  function SaveAdd(array $data) {
+   $_AccessCode = "Denied";
+   $_DIalog = [
+    "Body" => "The Member or Product Identifier is missing."
+   ];
    $data = $a["Data"] ?? [];
    $data = $this->core->DecodeBridgeData($data);
    $id = $data["Product"] ?? "";
-   $r = [
-    "Body" => "The Member or Product Identifier is missing."
-   ];
    $username = $data["Username"] ?? "";
    $shopID = $data["Shop"] ?? md5($username);
    $y = $this->you;
    $you = $y["Login"]["Username"];
-   if(!empty($id) && !empty($username)) {
-    $accessCode = "Accepted";
+   if($this->core->ID == $you) {
+    $_Dialog = [
+     "Body" => "You must sign in to continue.",
+     "Header" => "Forbidden"
+    ];
+   } elseif(!empty($id) && !empty($username)) {
+    $_AccessCode = "Accepted";
     $instructions = $data["Instructions"] ?? "";
     $username = $username ?? $you;
     $t = ($username == $you) ? $y : $this->core->Member($t);
@@ -220,7 +235,7 @@
     $cart["Products"][$id]["QTY"] = $cart["Products"][$id]["QTY"] + $quantity;
     $y["Shopping"]["Cart"][$shopID] = $cart;
     $this->core->Data("Save", ["mbr", md5($you), $y]);
-    $r = [
+    $_Dialog = [
      "Body" => "<em>$productTitle</em> was added to your cart for <em>$title</em>!",
      "Header" => "Added to Cart",
      "Options" => [
@@ -232,33 +247,26 @@
     ];
    }
    return $this->core->JSONResponse([
-    "AccessCode" => $accessCode,
-    "AddTopMargin" => "0",
-    "Response" => [
-     "JSON" => "",
-     "Web" => $r
-    ],
-    "ResponseType" => "Dialog",
+    "AccessCode" => $_AccessCode,
+    "Dialog" => $_Dialog,
     "Success" => "CloseCard"
    ]);
   }
-  function SaveRemove(array $a) {
-   $accessCode = "Denied";
+  function SaveRemove(array $data) {
+   $_Dialog = [
+    "Body" => "The Shop or Product Identifier are missing."
+   ];
    $data = $a["Data"] ?? [];
    $product = $data["Product"] ?? "";
    $shop = $data["Shop"] ?? "";
-   $r = [
-    "Body" => "The Shop or Product Identifier are missing."
-   ];
    $y = $this->you;
    $you = $y["Login"]["Username"];
    if($this->core->ID == $you) {
-    $r = [
+    $_Dialog = [
      "Body" => "You must sign in to continue.",
      "Header" => "Forbidden"
     ];
    } elseif(!empty($product) && !empty($shop)) {
-    $accessCode = "Accepted";
     $newProducts = [];
     $cart = $y["Shopping"]["Cart"][$shop] ?? [];
     $products = $cart["Products"] ?? [];
@@ -269,30 +277,22 @@
     }
     $cart["Products"] = $newProducts;
     $y["Shopping"]["Cart"][$shop] = $cart;
-    $r = [
+    $this->core->Data("Save", ["mbr", md5($you), $y]);
+    $_Dialog = [
      "Body" => "The Product was removed from your cart.",
      "Header" => "Done"
     ];
-    $this->core->Data("Save", ["mbr", md5($you), $y]);
    }
    return $this->core->JSONResponse([
-    "AccessCode" => $accessCode,
-    "AddTopMargin" => "0",
-    "Response" => [
-     "JSON" => "",
-     "Web" => $r
-    ],
-    "ResponseType" => "Dialog"
+    "Dialog" => $_Dialog
    ]);
   }
-  function Summary(array $a) {
-   $accessCode = "Accepted";
-   $data = $a["Data"] ?? [];
-   $data = $this->core->FixMissing($data, ["UN"]);
+  function Summary(array $data) {
+   $data = $data["Data"] ?? [];
    $y = $this->you;
    $you = $y["Login"]["Username"];
-   $username = $data["UN"];
-   $username = (!empty($username)) ? base64_decode($username) : $you;
+   $username = $data["UN"] ?? base64_encode($you);;
+   $username = base64_decode($username);
    $shopID = md5($username);
    $cart = $y["Shopping"]["Cart"][$shopID]["Products"] ?? [];
    $cartCount = count($cart);
@@ -300,11 +300,11 @@
    $credits = number_format($credits, 2);
    $discountCode = $y["Shopping"]["Cart"][$shopID]["DiscountCode"] ?? 0;
    $now = $this->core->timestamp;
-   $shop = $this->core->Data("Get", ["shop", $shopID]) ?? [];
+   $shop = $this->core->Data("Get", ["shop", $shopID]);
    $subtotal = 0;
    $total = 0;
    foreach($cart as $key => $value) {
-    $product = $this->core->Data("Get", ["product", $key]) ?? [];
+    $product = $this->core->Data("Get", ["product", $key]);
     $quantity = $product["Quantity"] ?? 0;
     if(!empty($product) && $quantity != 0) {
      $productIsActive = (strtotime($now) < $product["Expires"]) ? 1 : 0;
@@ -342,21 +342,17 @@
      "data-type" => "Checkout;".base64_encode("v=".base64_encode("Shop:Pay")."&Shop=$shopID&Type=Checkout&ViewPairID=".base64_encode("Checkout"))
     ]
    ]) : "";
-   $r = $this->core->Change([[
-    "[Cart.Continue]" => $continue,
-    "[Cart.Summary.Discount]" => number_format($credits + $discountCode, 2),
-    "[Cart.Summary.Subtotal]" => number_format($subtotal, 2),
-    "[Cart.Summary.Tax]" => number_format($tax, 2),
-    "[Cart.Summary.Total]" => number_format($tax + $total, 2)
-   ], $this->core->Extension("94eb319f456356da1d6e102670686a29")]);
    return $this->core->JSONResponse([
-    "AccessCode" => $accessCode,
-    "AddTopMargin" => "0",
-    "Response" => [
-     "JSON" => "",
-     "Web" => $r
-    ],
-    "ResponseType" => "View"
+    "View" => [
+     "ChangeData" => [
+      "[Cart.Continue]" => $continue,
+      "[Cart.Summary.Discount]" => number_format($credits + $discountCode, 2),
+      "[Cart.Summary.Subtotal]" => number_format($subtotal, 2),
+      "[Cart.Summary.Tax]" => number_format($tax, 2),
+      "[Cart.Summary.Total]" => number_format($tax + $total, 2)
+     ],
+     "ExtensionID" => "94eb319f456356da1d6e102670686a29"
+    ]
    ]);
   }
   function __destruct() {
