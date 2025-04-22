f4376c5a31b815b826fbbc7caf59172225622f5d
Outer Haven, LLC <117410279+OuterHavenLLC@users.noreply.github.com>
2025-04-22 01:27:02 -0400

    The Discount Codes view group was updated to current standards.

--- base/views/DiscountCode.php
+++ base/views/DiscountCode.php
@@ -4,20 +4,14 @@
    parent::__construct();
    $this->you = $this->core->Member($this->core->Authenticate("Get"));
   }
-  function Edit(array $a) {
-   $accessCode = "Accepted";
-   $data = $a["Data"] ?? [];
+  function Edit(array $data) {
+   $data = $data["Data"] ?? [];
    $new = $data["new"] ?? 0;
    $y = $this->you;
    $you = $y["Login"]["Username"];
    $id = $data["ID"] ?? md5($you."_DC_".$this->core->timestamp);
    $action = ($new == 1) ? "Post" : "Update";
-   $action = $this->core->Element(["button", $action, [
-    "class" => "CardButton SendData",
-    "data-form" => ".Discount$id",
-    "data-processor" => base64_encode("v=".base64_encode("DiscountCode:Save"))
-   ]]);
-   $discount = $this->core->Data("Get", ["dc", md5($you)]) ?? [];
+   $discount = $this->core->Data("Get", ["dc", md5($you)]);
    $discount = $discount[$id] ?? [];
    $code = $discount["Code"] ?? base64_encode("");
    $dollarAmount = $discount["DollarAmount"] ?? 1.00;
@@ -30,57 +24,55 @@
    } for($i = 1; $i < 100; $i++) {
     $quantities[$i] = $i;
    }
-   $r = $this->core->Change([[
-    "[Discount.Code]" => $code,
-    "[Discount.DollarAmount]" => $dollarAmount,
-    "[Discount.ID]" => $id,
-    "[Discount.New]" => $new,
-    "[Discount.Percentages]" => json_encode($percentages, true),
-    "[Discount.Percentile]" => $percentile,
-    "[Discount.Quantities]" => json_encode($quantities, true),
-    "[Discount.Quantity]" => $quantity
-   ], $this->core->Extension("47e35864b11d8bdc255b0aec513337c0")]);
-   $r = [
-    "Action" => $action,
-    "Front" => $r
-   ];
    return $this->core->JSONResponse([
-    "AccessCode" => $accessCode,
-    "AddTopMargin" => "0",
-    "Response" => [
-     "JSON" => "",
-     "Web" => $r
-    ],
-    "ResponseType" => "View"
+    "Card" => [
+     "Action" => $this->core->Element(["button", $action, [
+      "class" => "CardButton SendData",
+      "data-form" => ".Discount$id",
+      "data-processor" => base64_encode("v=".base64_encode("DiscountCode:Save"))
+     ]]),
+     "Front" => [
+      "ChangeData" => [
+       "[Discount.Code]" => $code,
+       "[Discount.DollarAmount]" => $dollarAmount,
+       "[Discount.ID]" => $id,
+       "[Discount.New]" => $new,
+       "[Discount.Percentages]" => json_encode($percentages, true),
+       "[Discount.Percentile]" => $percentile,
+       "[Discount.Quantities]" => json_encode($quantities, true),
+       "[Discount.Quantity]" => $quantity
+      ],
+      "ExtensionID" => "47e35864b11d8bdc255b0aec513337c0"
+     ]
+    ]
    ]);
   }
-  function Purge(array $a) {
-   $accessCode = "Denied";
-   $data = $a["Data"] ?? [];
+  function Purge(array $data) {
+   $_Dialog = [
+    "Body" => "The Code Identifier is missing."
+   ];
+   $_View = "";
+   $data = $data["Data"] ?? [];
    $key = $data["Key"] ?? base64_encode("");
    $key = base64_decode($key);
    $id = $data["ID"] ?? "";
-   $r = [
-    "Body" => "The Code Identifier is missing.",
-    "Header" => "Error"
-   ];
    $secureKey = $data["SecureKey"] ?? base64_encode("");
    $secureKey = base64_decode($secureKey);
    $y = $this->you;
    $you = $y["Login"]["Username"];
    if(md5($key) != $y["Login"]["PIN"]) {
-    $r = [
+    $_Dialog = [
      "Body" => "The PINs do not match."
     ];
    } elseif($this->core->ID == $you) {
-    $r = [
+    $_Dialog = [
      "Body" => "You must be signed in to continue.",
      "Header" => "Forbidden"
     ];
    } elseif(!empty($id)) {
-    $accessCode = "Accepted";
+    $_Dialog = "";
     $id = base64_decode($id);
-    $discounts = $this->core->Data("Get", ["dc", md5($you)]) ?? [];
+    $discounts = $this->core->Data("Get", ["dc", md5($you)]);
     $newDiscounts = [];
     foreach($discounts as $key => $value) {
      if($id != $key) {
@@ -89,7 +81,7 @@
     }
     $discounts = $newDiscounts;
     $this->core->Data("Save", ["dc", md5($you), $newDiscounts]);
-    $r = $this->core->Element([
+    $_View = $this->core->Element([
      "p", "The Discount Code was removed.",
      ["class" => "CenterText"]
     ]).$this->core->Element([
@@ -97,18 +89,16 @@
     ]);
    }
    return $this->core->JSONResponse([
-    "AccessCode" => $accessCode,
-    "AddTopMargin" => "0",
-    "Response" => [
-     "JSON" => "",
-     "Web" => $r
-    ],
-    "ResponseType" => "View"
+    "Dialog" => $_Dialog,
+    "View" => $_View
    ]);
   }
-  function Save(array $a) {
-   $accessCode = "Denied";
-   $data = $a["Data"] ?? [];
+  function Save(array $data) {
+   $_AccessCode = "Denied";
+   $_Dialog = [
+    "Body" => "The Code Identifier is missing."
+   ];
+   $data = $data["Data"] ?? [];
    $data = $this->core->DecodeBridgeData($data);
    $data = $this->core->FixMissing($data, [
     "DC",
@@ -118,32 +108,28 @@
     "Quantity"
    ]);
    $new = $data["New"] ?? 0;
-   $r = [
-    "Body" => "The Code Identifier is missing."
-   ];
    $y = $this->you;
    $you = $y["Login"]["Username"];
    if($this->core->ID == $you) {
-    $r = [
+    $_Dialog = [
      "Body" => "You must be signed in to continue.",
      "Header" => "Forbidden"
     ];
    } elseif(!empty($data["ID"])) {
-    $r = [
-     "Body" => "The Code is missing.",
-     "Header" => "Error"
+    $_Dialog = [
+     "Body" => "The Code is missing."
     ];
     if(!empty($data["DC"])) {
-     $accessCode = "Accepted";
+     $_AccessCode = "Accepted";
      $actionTaken = ($new == 1) ? "posted" : "updated";
-     $discount = $this->core->Data("Get", ["dc", md5($you)]) ?? [];
+     $discount = $this->core->Data("Get", ["dc", md5($you)]);
      $discount[$data["ID"]] = [
       "Code" => base64_encode($data["DC"]),
       "DollarAmount" => $data["DollarAmount"],
       "Percentile" => $data["Percentile"],
       "Quantity" => $data["DiscountCodeQTY"]
      ];
-     $r = [
+     $_Dialog = [
       "Body" => "The Code <em>".$data["DC"]."</em> was $actionTaken!",
       "Header" => "Done"
      ];
@@ -151,13 +137,8 @@
     }
    }
    return $this->core->JSONResponse([
-    "AccessCode" => $accessCode,
-    "AddTopMargin" => "0",
-    "Response" => [
-     "JSON" => "",
-     "Web" => $r
-    ],
-    "ResponseType" => "Dialog",
+    "AccessCode" => $_AccessCode,
+    "Dialog" => $_Dialog,
     "Success" => "CloseCard"
    ]);
   }
