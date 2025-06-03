e3022a7a663fe8c075706a2d6187e3c6e90c39be
Outer Haven, LLC <117410279+OuterHavenLLC@users.noreply.github.com>
2025-06-03 09:44:34 -0400

    View Groups now redirect to the base URL if the Outer Haven (OH) class is not found.

--- base/views/Share.php
+++ base/views/Share.php
@@ -1,4 +1,10 @@
 <?php
+ if(!class_exists("OH")) {
+  $protocol = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] === "on") ? "https://" : "http://";
+  $host = $protocol . $_SERVER["HTTP_HOST"]."/";
+  header("Location: $host");
+  exit;
+ }
  Class Share extends OH {
   function __construct() {
    parent::__construct();
