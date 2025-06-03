1f9baa833e606ca415cf23db623a76b0aecd119e
Outer Haven, LLC <117410279+OuterHavenLLC@users.noreply.github.com>
2025-06-03 10:36:48 -0400

    Standardized more redirects.

--- base/pay/index.php
+++ base/pay/index.php
@@ -1,3 +1,5 @@
 <?php
- header("Location: http://".$_SERVER["HTTP_HOST"]);
+ $protocol = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] === "on") ? "https://" : "http://";
+ $host = $protocol.$_SERVER["HTTP_HOST"]."/";
+ header("Location: $host");
 ?>
