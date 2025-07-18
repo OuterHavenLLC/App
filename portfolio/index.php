9db519a62faa17f3250fc50fb17c34f1b436c3a9
Mike <Miguel.Nieves.NYC@icloud.com>
2025-07-18 02:53:40 -0400

    Synchronization Check II

--- portfolio/index.php
+++
@@ -1,5 +0,0 @@
-<?php
- $protocol = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] === "on") ? "https://" : "http://";
- $host = $protocol.$_SERVER["HTTP_HOST"]."/";
- header("Location: $host");
-?>
